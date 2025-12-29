efb093378c4c49f2d376adb4404d89be78bf45ba
@vanshkansal25 <kansalv578@gmail.com>
2025-12-29 17:06:58 +0530

    feat:Implemented updateCartQuantity and removeFromCart functionality

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -2,22 +2,16 @@ import { NextFunction, Request, Response } from "express";
 import { asyncHandler } from "../utils/asyncHandler";
 import { db } from "../db/db";
 import { cart_items, carts } from "../db/schemas";
-import { eq, sql } from "drizzle-orm";
+import { and, eq, sql } from "drizzle-orm";
 import { ApiResponse } from "../utils/apiResponse";
 import { redis } from "../utils/redis";
-
-
-
-
-
-
-
+import { ApiError } from "src/utils/apiError";
 // add cart
 export const addCart = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     const { variantId, quantity } = req.body;
     const userId = req.user?.id;
     if (!userId) {
-        return next(new Error("Unauthorized"));
+        return next(new ApiError(401, "Unauthorized"));
     }
     const cacheKey = `cart:${userId}`
     const cart = await db.transaction(async (tx) => {
@@ -52,14 +46,78 @@ export const addCart = asyncHandler(async (req: Request, res: Response, next: Ne
         new ApiResponse(200, cart, "Item added to cart successfully")
     );
 })
-// removeItemFromCart
 // updateItemFromCart
-// getCart
+export const updateCartQuantity = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const { variantId, quantity } = req.body; // New absolute quantity (e.g., set to 5)
+    const userId = req.user?.id;
+    if (!userId) {
+        return next(new ApiError(401, "Unauthorized"));
+    }
 
+    if (quantity < 1) {
+        throw new ApiError(400, "Quantity must be at least 1. Use remove to delete item.");
+    }
+    await db.transaction(async (tx) => {
+        const userCart = await tx.query.carts.findFirst({
+            where: eq(carts.userId, userId),
+        });
+
+        if (!userCart) throw new ApiError(404, "Cart not found");
+        const updatedItem = await tx
+            .update(cart_items)
+            .set({ quantity: quantity })
+            .where(
+                and(
+                    eq(cart_items.cartId, userCart.id),
+                    eq(cart_items.variantId, variantId)
+                )
+            )
+            .returning();
+
+        if (updatedItem.length === 0) {
+            throw new ApiError(404, "Item not found in cart");
+        }
+        await redis.del(`cart:${userId}`);
+    });
+
+    return res.status(200).json(new ApiResponse(200, {}, "Cart quantity updated"));
+});
+// removeItemFromCart
+export const removeFromCart = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const { variantId } = req.params;
+    const userId = req.user?.id;
+    if (!userId) {
+        return next(new ApiError(401, "Unauthorized"));
+    }
+    await db.transaction(async (tx) => {
+        const userCart = await tx.query.carts.findFirst({
+            where: eq(carts.userId, userId),
+        });
+
+        if (!userCart) throw new ApiError(404, "Cart not found");
+
+        const deleted = await tx
+            .delete(cart_items)
+            .where(
+                and(
+                    eq(cart_items.cartId, userCart.id),
+                    eq(cart_items.variantId, variantId)
+                )
+            )
+            .returning();
+        if (deleted.length === 0) {
+            throw new ApiError(404, "Item not found in cart");
+        }
+        await redis.del(`cart:${userId}`);
+    });
+
+    return res.status(200).json(new ApiResponse(200, {}, "Item removed from cart"));
+});
+// getCart
 export const getCart = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     const userId = req.user?.id;
     if (!userId) {
-        return next(new Error("Unauthorized"));
+        return next(new ApiError(401, "Unauthorized"));
     }
 
     const cacheKey = `cart:${userId}`;
--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { addCart, getCart } from "../controllers/cart.controller";
+import { addCart, getCart, removeFromCart, updateCartQuantity } from "../controllers/cart.controller";
 
 const cartRouter = Router();
 
 
 cartRouter.post("/add-cart", authMiddleware, addCart);
+cartRouter.patch("/update-cart", authMiddleware, updateCartQuantity);
+cartRouter.delete("/remove-from-cart/:variantId", authMiddleware, removeFromCart);
 cartRouter.get("/get-cart", authMiddleware, getCart);
 
 
